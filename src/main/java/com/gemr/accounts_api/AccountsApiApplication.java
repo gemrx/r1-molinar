518683ed0612f5f5db215412c42c745071bc7ab3
gemr <gemrdev@gmail.com>
2025-01-31 22:44:20 -0500

    Initial project setup

---
+++ src/main/java/com/gemr/accounts_api/AccountsApiApplication.java
@@ -0,0 +1,13 @@
+package com.gemr.accounts_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApiApplication.class, args);
+	}
+
+}
